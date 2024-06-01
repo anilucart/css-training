a3354d7bf7121c2b719254a29726d87bb0fdd7c9
ww <ww@wws-MacBook-Air.local>
2024-06-01 19:23:52 +0300

    ff

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function activelink() {
   this.classList.add("active");
 }
 list.forEach((item) => item.addEventListener("click", activelink));
+
+// const userName ='vlad'
+
+// function createUserName(name)
+// {return name.toUpperCase()}
+
+// const upDatedName = createUserName(userName) //я просто створив змінну в якій помістив значення для легкого написання 
+
+// console.log(upDatedName );
+
+
+// function declaration(greettings){ // function declaration 
+// alert(greettings);
+// }
+
+// const expression = function(greettings){//function expression
+//   alert(greettings);
+// }
+
+// const car={
+//   spead: 100,
+//   color: 'red',
+//   drive: function(){ // це називається методом в донному випадку drive це вже метод
+//   console.log('drive very fast')
+//   }
+// }
+// car.drive() //виклик 
+
